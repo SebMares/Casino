deded32e15543e0921b26eff5ad90b3b2a4d13b0
SebMares <91852663+SebMares@users.noreply.github.com>
2025-02-04 22:37:23 -0600

    Update guessingGame.cpp
    added the code for the guessing game

--- guessingGame.cpp
+++ guessingGame.cpp
@@ -6,5 +6,21 @@
 using namespace std;
 
 void guessingGame() {
+    int guess;
+    int number;
+    srand(time(0));
+    number = rand() % 100 + 1;
 
+    cout << "Welcome to the Guessing Game!\n";
+    cout << "Guess a number between 1 and 100: ";
+    cin >> guess;
+
+    if (guess == number) 
+    {
+        cout << "Congratulations! You guessed it right!\n";
+    }
+    else
+    {
+        cout << "Sorry, the number was " << number << ". Better luck next time!\n";
+    }
 }
